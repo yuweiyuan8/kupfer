69036d008bb04a39098330633e637d8e2635f779
InsanePrawn <insane.prawny@gmail.com>
2022-08-30 15:51:55 +0200

    wrapper/docker: fix indentation (only version needs to be pulled from `with open():`)

--- wrapper/docker.py
+++ wrapper/docker.py
@@ -25,71 +25,71 @@ class DockerWrapper(BaseWrapper):
         assert script_path
         with open(os.path.join(script_path, 'version.txt')) as version_file:
             version = version_file.read().replace('\n', '')
-            tag = f'registry.gitlab.com/kupfer/kupferbootstrap:{version}'
-            if version == 'dev':
-                logging.info(f'Building docker image "{tag}"')
-                cmd = [
-                    'docker',
-                    'build',
-                    '.',
-                    '-t',
-                    tag,
-                ] + (['-q'] if not config.runtime.verbose else [])
-                logging.debug('Running docker cmd: ' + ' '.join(cmd))
-                result = subprocess.run(cmd, cwd=script_path, capture_output=True)
-                if result.returncode != 0:
-                    logging.fatal('Failed to build docker image:\n' + result.stderr.decode())
-                    exit(1)
-            else:
-                # Check if the image for the version already exists
-                result = subprocess.run(
-                    [
-                        'docker',
-                        'images',
-                        '-q',
-                        tag,
-                    ],
-                    capture_output=True,
-                )
-                if result.stdout == b'':
-                    logging.info(f'Pulling kupferbootstrap docker image version \'{version}\'')
-                    subprocess.run([
-                        'docker',
-                        'pull',
-                        tag,
-                    ])
-            container_name = f'kupferbootstrap-{self.uuid}'
-
-            wrapped_config = self.generate_wrapper_config()
-
-            target_user = 'root' if config.runtime.uid == 0 else 'kupfer'
-            target_home = '/root' if target_user == 'root' else f'/home/{target_user}'
-
-            ssh_dir = os.path.join(pathlib.Path.home(), '.ssh')
-            if not os.path.exists(ssh_dir):
-                os.makedirs(ssh_dir, mode=0o700)
-            volumes = self.get_bind_mounts_default(wrapped_config, ssh_dir=ssh_dir, target_home=target_home)
-            volumes |= dict({config.get_path(vol_name): vol_dest for vol_name, vol_dest in DOCKER_PATHS.items()})
-            docker_cmd = [
+        tag = f'registry.gitlab.com/kupfer/kupferbootstrap:{version}'
+        if version == 'dev':
+            logging.info(f'Building docker image "{tag}"')
+            cmd = [
                 'docker',
-                'run',
-                '--name',
-                container_name,
-                '--rm',
-                '--interactive',
-                '--tty',
-                '--privileged',
-            ] + docker_volumes_args(volumes) + [tag]
+                'build',
+                '.',
+                '-t',
+                tag,
+            ] + (['-q'] if not config.runtime.verbose else [])
+            logging.debug('Running docker cmd: ' + ' '.join(cmd))
+            result = subprocess.run(cmd, cwd=script_path, capture_output=True)
+            if result.returncode != 0:
+                logging.fatal('Failed to build docker image:\n' + result.stderr.decode())
+                exit(1)
+        else:
+            # Check if the image for the version already exists
+            result = subprocess.run(
+                [
+                    'docker',
+                    'images',
+                    '-q',
+                    tag,
+                ],
+                capture_output=True,
+            )
+            if result.stdout == b'':
+                logging.info(f'Pulling kupferbootstrap docker image version \'{version}\'')
+                subprocess.run([
+                    'docker',
+                    'pull',
+                    tag,
+                ])
+        container_name = f'kupferbootstrap-{self.uuid}'
 
-            kupfer_cmd = ['kupferbootstrap', '--config', volumes[wrapped_config]] + self.filter_args_wrapper(sys.argv[1:])
-            if config.runtime.uid:
-                kupfer_cmd = ['wrapper_su_helper', '--uid', str(config.runtime.uid), '--username', 'kupfer', '--'] + kupfer_cmd
+        wrapped_config = self.generate_wrapper_config()
 
-            cmd = docker_cmd + kupfer_cmd
-            logging.debug('Wrapping in docker:' + repr(cmd))
-            result = subprocess.run(cmd)
+        target_user = 'root' if config.runtime.uid == 0 else 'kupfer'
+        target_home = '/root' if target_user == 'root' else f'/home/{target_user}'
 
-            exit(result.returncode)
+        ssh_dir = os.path.join(pathlib.Path.home(), '.ssh')
+        if not os.path.exists(ssh_dir):
+            os.makedirs(ssh_dir, mode=0o700)
+        volumes = self.get_bind_mounts_default(wrapped_config, ssh_dir=ssh_dir, target_home=target_home)
+        volumes |= dict({config.get_path(vol_name): vol_dest for vol_name, vol_dest in DOCKER_PATHS.items()})
+        docker_cmd = [
+            'docker',
+            'run',
+            '--name',
+            container_name,
+            '--rm',
+            '--interactive',
+            '--tty',
+            '--privileged',
+        ] + docker_volumes_args(volumes) + [tag]
+
+        kupfer_cmd = ['kupferbootstrap', '--config', volumes[wrapped_config]] + self.filter_args_wrapper(sys.argv[1:])
+        if config.runtime.uid:
+            kupfer_cmd = ['wrapper_su_helper', '--uid', str(config.runtime.uid), '--username', 'kupfer', '--'] + kupfer_cmd
+
+        cmd = docker_cmd + kupfer_cmd
+        logging.debug('Wrapping in docker:' + repr(cmd))
+        result = subprocess.run(cmd)
+
+        exit(result.returncode)
 
     def stop(self):
         subprocess.run(
