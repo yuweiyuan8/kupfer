f705e39ca1fdeae4382cb22d6d220fb9199207ba
InsanePrawn <insane.prawny@gmail.com>
2022-09-12 04:56:06 +0200

    chroot.mount_{ccache,rust}: use config.get_path() to resolve %cache_dir%

--- chroot/build.py
+++ chroot/build.py
@@ -133,7 +133,7 @@ class BuildChroot(Chroot):
         )
 
     def mount_ccache(self, user: str = 'kupfer', fail_if_mounted: bool = False):
-        mount_source = os.path.join(config.file.paths.ccache, self.arch)
+        mount_source = os.path.join(config.get_path('ccache'), self.arch)
         mount_dest = os.path.join(f'/home/{user}' if user != 'root' else '/root', '.ccache')
         uid = self.get_uid(user)
         makedir(mount_source, user=uid)
@@ -146,7 +146,7 @@ class BuildChroot(Chroot):
     def mount_rust(self, user: str = 'kupfer', fail_if_mounted: bool = False) -> list[str]:
         results = []
         uid = self.get_uid(user)
-        mount_source_base = config.file.paths.rust  # apparently arch-agnostic
+        mount_source_base = config.get_path('rust')  # apparently arch-agnostic
         for rust_dir in ['cargo', 'rustup']:
             mount_source = os.path.join(mount_source_base, rust_dir)
             mount_dest = os.path.join(f'/home/{user}' if user != 'root' else '/root', f'.{rust_dir}')
